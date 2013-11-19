4459b2d6dbf7302ddb2356c4edc42f3acefab60e
Jean Ponchon <jean.ponchon@novadiscovery.com>
2013-11-19 12:05:31 +0100

    Initial commit

---
+++ tasks/transfo.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-transfo
+ * https://github.com/nopnop/grunt-transfo
+ *
+ * Copyright (c) 2013 Jean Ponchon
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('transfo', 'Streamed copy and/or concat with stream transformation pipeline  (compatible with  ccontrib-copy/concat)', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
